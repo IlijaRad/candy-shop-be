b29d182fa664369cb1f573a21aadd507cd4846b8
IlijaRad <ilija.radivojevic237@gmail.com>
2025-07-29 19:13:23 +0200

    WIP

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+// use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegistrationController;
+
+Route::post('register', [RegistrationController::class, 'store']);
+Route::post('login', [AuthController::class, 'store']);
+
+Route::group([
+    "middleware" => 'auth:sanctum'
+], function () {
+    Route::get("user", [AuthController::class, "show"]);
+    Route::delete("logout", [AuthController::class, "destroy"]);
+    // Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, 'store']);
+});
+
+Route::group([
+    "middleware" => 'guest'
+], function () {
+    // Route::post("reset-password", [NewPasswordController::class, "store"]);
+    // Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+    //     ->name('password.email');
+});
